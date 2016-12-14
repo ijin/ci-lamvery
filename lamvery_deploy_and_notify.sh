cc1cb04ac644149be0234db62b0958c315fa29f1
Michael H. Oshita <ijinpublic+github@gmail.com>
2016-12-15 03:55:30 +0900

    multiple envs

--- lamvery_deploy_and_notify.sh
+++ lamvery_deploy_and_notify.sh
@@ -7,18 +7,21 @@ export ENV=`echo $1 | rev | cut -d \- -f1 | rev`
 if [$1 -eq 'production']; then
     export ENV_PREFIX=$PROD_PREFIX
     export ROLE=$PROD_ROLE
+elif [$1 -eq 'staging']; then
+    export ENV_PREFIX=$STG_PREFIX
+    export ROLE=$STG_ROLE
 fi
 
 lamvery deploy -s --dry-run
 
 if [ $? -eq 0 ]; then
     export SL_COLOR="good"
-    export SL_TEXT="Deploy success: $S3 - ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
+    export SL_TEXT="Deploy success: $CIRCLE_PROJECT_REPONAME - ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
     export SL_ICON="https://s3-us-west-2.amazonaws.com/slack-files2/bot_icons/2015-08-11/8956311813_48.png"
     export EXIT=0
 else
     export SL_COLOR="danger"
-    export SL_TEXT="Sync failure: $S3 - ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
+    export SL_TEXT="Deploy failure: $CIRCLE_PROJECT_REPONAME - ${CIRCLE_BRANCH} (<${CIRCLE_COMPARE_URL}|${SHA1}>) by ${CIRCLE_USERNAME} !!"
     export SL_ICON="https://s3-us-west-2.amazonaws.com/slack-files2/bot_icons/2015-08-11/8956311813_48.png"
     export EXIT=1
 fi
